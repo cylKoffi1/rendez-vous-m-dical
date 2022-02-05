c497e21a30abd214ee2027196215b547d5e2dc06
cylKoffi1 <98591700+cylKoffi1@users.noreply.github.com>
2022-02-05 13:37:55 +0000

    je viens de modifier encore

--- Admin/horaire.php
+++ Admin/horaire.php
@@ -1,98 +1,78 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-  	<title>Calendar 04</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" href="../Admin/calendar-04/css/style.css">
-
-	</head>
-	<body>
-	<section class="ftco-section">
-		<div class="container">
-		<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">PLANIFICATION DE L'EMPLOI DU TEMPS </h2>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="content w-100">
-				    <div class="calendar-container">
-				      <div class="calendar"> 
-				        <div class="year-header"> 
-				          <span class="left-button fa fa-chevron-left" id="prev"> </span> 
-				          <span class="year" id="label"></span> 
-				          <span class="right-button fa fa-chevron-right" id="next"> </span>
-				        </div> 
-				        <table class="months-table w-100"> 
-				          <tbody>
-				            <tr class="months-row">
-				              <td class="month">Jan</td> 
-				              <td class="month">Feb</td> 
-				              <td class="month">Mar</td> 
-				              <td class="month">Apr</td> 
-				              <td class="month">May</td> 
-				              <td class="month">Jun</td> 
-				              <td class="month">Jul</td>
-				              <td class="month">Aug</td> 
-				              <td class="month">Sep</td> 
-				              <td class="month">Oct</td>          
-				              <td class="month">Nov</td>
-				              <td class="month">Dec</td>
-				            </tr>
-				          </tbody>
-				        </table> 
-				        
-				        <table class="days-table w-100"> 
-				          <td class="day">Sun</td> 
-				          <td class="day">Mon</td> 
-				          <td class="day">Tue</td> 
-				          <td class="day">Wed</td> 
-				          <td class="day">Thu</td> 
-				          <td class="day">Fri</td> 
-				          <td class="day">Sat</td>
-				        </table> 
-				        <div class="frame"> 
-				          <table class="dates-table w-100"> 
-			              <tbody class="tbody">             
-			              </tbody> 
-				          </table>
-				        </div> 
-				        <button class="button" id="add-button">Valider</button>
-				      </div>
-				    </div>
-				    <div class="events-container">
-				    </div>
-				    <div class="dialog" id="dialog">
-				        <h2 class="dialog-header"> Ajouter votre programme </h2>
-				        <form class="form" id="form"><center>
-				          <div class="form-container" align="center">
-				            <label class="form-label" id="valueFromMyButton" for="name">TITRE</label>
-				            <input class="input" type="text" id="name" maxlength="36">
-				            <label class="form-label" id="valueFromMyButton" for="count">HEURE</label>
-				            <input class="input" type="time" id="count" min="0" max="1000000" maxlength="7">
-				            <input type="button" value="Cancel" class="button" id="cancel-button">
-				            <input type="button" value="OK" class="button button-white" id="ok-button"></center>  
-				          </div>
-				        </form>
-				      </div>
-				  </div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<script src="../Admin/calendar-04/js/jquery.min.js"></script>
-  <script src="../Admin/calendar-04/js/popper.js"></script>
-  <script src="../Admin/calendar-04/js/bootstrap.min.js"></script>
-  <script src="../Admin/calendar-04/js/main.js"></script>
-
-	</body>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../assets/css/calender.css">
+</head>
+<body>
+<div class="calendar" id="calendar-app">
+  <div class="calendar--day-view" id="day-view">
+    <span class="day-view-exit" id="day-view-exit">&times;</span>
+    <span class="day-view-date" id="day-view-date">MAI 29 2016</span>
+    <div class="day-view-content">
+      <div class="day-highlight">
+        Tu <span class="day-events" id="day-events">N'as aucun évènement aujourd'hui</span>. &nbsp; <span tabindex="0" onkeyup="if(event.keyCode != 13) return; this.click();" class="day-events-link" id="add-event" data-date>Add a new event?</span>
+      </div>
+      <div class="day-add-event" id="add-day-event-box" data-active="false">
+        <div class="row">
+          <div class="half">
+            <label class="add-event-label">
+               Nom de l'évènement
+              <input type="text" class="add-event-edit add-event-edit--long" placeholder="New event" id="input-add-event-name">
+             
+            </label>
+          </div>
+          <div class="qtr">
+            <label class="add-event-label">
+          Début de l'heure
+              <input type="text" class="add-event-edit" placeholder="8:15" id="input-add-event-start-time" data-options="1,2,3,4,5,6,7,8,9,10,11,12" data-format="datetime">
+              <input type="text" class="add-event-edit" placeholder="am" id="input-add-event-start-ampm" data-options="a,p,am,pm">
+            </label>
+          </div>
+          <div class="qtr">
+            <label class="add-event-label">
+          Fin de l'heure
+              <input type="text" class="add-event-edit" placeholder="9" id="input-add-event-end-time" data-options="1,2,3,4,5,6,7,8,9,10,11,12" data-format="datetime">
+              <input type="text" class="add-event-edit" placeholder="am" id="input-add-event-end-ampm" data-options="a,p,am,pm">
+            </label>
+          </div>
+          <div class="half">
+            <a onkeyup="if(event.keyCode != 13) return; this.click();" tabindex="0" id="add-event-save" class="event-btn--save event-btn">save</a>
+            <a tabindex="0" id="add-event-cancel" class="event-btn--cancel event-btn">retour</a>
+          </div>
+        </div>
+        
+      </div>
+      <div id="day-events-list" class="day-events-list">
+        
+      </div>
+      <div class="day-inspiration-quote" id="inspirational-quote">
+        Every child is an artist.  The problem is how to remain an artist once he grows up. –Pablo Picasso
+      </div>
+    </div>
+  </div>
+  <div class="calendar--view" id="calendar-view">
+    <div class="cview__month">
+      <span class="cview__month-last" id="calendar-month-last">Avr</span>
+      <span class="cview__month-current" id="calendar-month">Mai</span>
+      <span class="cview__month-next" id="calendar-month-next">Jui</span>
+    </div>
+    <div class="cview__header">Dim</div>
+    <div class="cview__header">Lun</div>
+    <div class="cview__header">Mar</div>
+    <div class="cview__header">Mer</div>
+    <div class="cview__header">Jeu</div>
+    <div class="cview__header">Ven</div>
+    <div class="cview__header">Sam</div>
+    <div class="calendar--view" id="dates">
+    </div>
+  </div>
+  <div class="footer">
+    <span><span id="footer-date" class="footer__link">Aujourd'hui est le 30 Mai</span></span>    
+  </div>
+</div>
+<script src="../assets/js/calender.js"></script>
+</body>
 </html>
-
